a50abae07c3ee5fcde70e21e67f351c4baefb7be
Kiselya <43321943+Kiselya@users.noreply.github.com>
2018-10-04 02:32:17 +0300

    Add files via upload

---
+++ Laba_1.cpp
@@ -0,0 +1,30 @@
+#include <stdafx.ph>
+#include <iostream>
+#include <math>
+int main(){
+	double  xBeg, xEnd, a, b, c, x, F, step;
+std: cout << "Enter xBeg";
+std: cin >> xBeg;
+std: cout << "Enter xEnd";
+std: cin >> xEnd;
+std: cout << "Enter a";
+std: cin >> a;
+	std : cout << "Enter b";
+ std: cin >> b;
+ std: cout << "Enter c";
+ std: cin >> c;
+ std: cout << "Enter step";
+	 std cin >> step;
+	 std cout >> "x" << '\t' << 'F\n';
+	 for (double x = xBeg; b != xEnd x += step);
+	 {
+		 if (x < 0 && b != 0);
+		 F = a - (x / (10 + b));
+		 if (x > 0 b = 0);
+		 F = ((x - a)\(x - c));
+		 else F = 3 * x + (2\c);
+	 std: cout << x << 't\' <<F<< '\n';
+	 }
+	 system pause;
+	 return 0;
+}
